632e8d98d3c49aef2e753892b18d05f66b1fca2c
M Usman <m_usman010@yahoo.com>
2023-06-20 10:37:26 +0100

    Update
    Added Notes For Inline functions and Noreturn functions.

--- AdvC/Advance_C/src/Advance_C.c
+++ AdvC/Advance_C/src/Advance_C.c
@@ -11,6 +11,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 #include <complex.h>
 
 
@@ -24,15 +25,51 @@ static struct{
 }Person;
 
 
-
 void add(){
 
 printf("\n In main thread a = %i",a);
 }
 
+//typedef int dty;
+
+
+
+
+int sum(int v1,...) {
+va_list parg;
+int val = 0,sum = 0,cnt=0;
+va_start(parg,v1);
+while(cnt<v1){
+	val = va_arg(parg,int);
+	sum +=val;
+	cnt+=1;
+}
+va_end(parg);
+
+return sum;
+}
+
+
+
+/*double average(double v1,double v2,...) {
+va_list parg;
+double sum = v1 + v2;
+double value = 0.0;
+int cnt = v1;
+va_start(parg,v2);
+
+while((value = va_arg(parg,double)) != 0.0) {
+  sum += value;
+  ++cnt;
+}
+va_end(parg);
+
+return sum/cnt;
+}*/
 
 int main(void) {
-	int cnt=1;
+
+	/*int cnt=1;
 	char *ptr = "Hello Usman";
 	char arr[] = "Hello Usman";
 	typedef int aa;
@@ -57,7 +94,7 @@ printf("size of array = %zu",sizeof(arr));
 printf("\n %c",*(arr+1));
 printf("\n %zu",strlen(ptr));
 
-/*********Complex Numbers*********/
+//*********Complex Numbers*********
 
 #ifdef __STDC_NO_COMPLEX__
  printf ("\n Complex numbers are not supported. \n");
@@ -72,20 +109,37 @@ printf("\n %zu",strlen(ptr));
 #endif
 
 
- /*******Type Qualifiers********/
+ //*******Type Qualifiers********
 
 const int a = 1;
 const int ptr1 = &a;
-printf("\n const pointer address = %p",ptr1);
+printf("\n const pointer address = %p \n ",ptr1);
+
+volatile int b;
+
+//*** Advanced Control Flow *********
+
+int i=(5,10);
+label : printf("\n Hello");
+
+goto label;
+	return EXIT_SUCCESS;*/
+
+/*putchar('C');
+
+double v1= 10.5, v2 = 2.5;
+int num1 = 6, num2 = 5;
+long num3 = 12L, num4 = 20L;*/
+
+//printf("Average = %.2lf\n@",average(v1,3.5,v2,4.5,0.0));
+
+
+printf("\n Using variadic function Sum = %i",sum(2,2,4));
+//printf("\n Average = %.2lf",average(v1,3.5,v2,4.5,0.0));
 
 
 
-
-	return EXIT_SUCCESS;
 }
-
-
-
 
 
 
